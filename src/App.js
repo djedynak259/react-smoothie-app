4ba064c952a850117e9c47cc7631cd58e8ce4161
djedynak259 <dajed@outlook.com>
2017-06-12 22:11:29 -0700

    string fix

--- src/App.js
+++ src/App.js
@@ -144,11 +144,13 @@ class SearchBar extends React.Component {
 class Recipe extends React.Component {
   render () {
     let string = '';
+    let count = 0;
     for (var i=0;i<this.props.recipe.length;i++) {
-      if(this.props.recipe.length === 1) {
+      if(count===0) {
         string = `${this.props.recipe[i]}`;  
+        count ++;
       } else {
-       string = `${string}, ${this.props.recipe[i]}`;
+        string = `${string}, ${this.props.recipe[i]}`;
       }
     }
 
