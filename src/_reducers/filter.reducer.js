7744468b717582a8d61255f6c3170904d6fbf288
djedynak259 <dajed@outlook.com>
2017-12-05 08:43:27 -0800

    reducer cleanerup

--- src/_reducers/filter.reducer.js
+++ src/_reducers/filter.reducer.js
@@ -4,11 +4,15 @@ let init = {
 }
 
 export function filter(state = init, action) {
+
   switch (action.type) {
+    
     case 'FILTER_INGREDIENTS' :
       return Object.assign({}, state, {filterText: action.filterText})
+
     case 'RECENT_SELECTION' :
       return Object.assign({}, state, {recentSelection: action.name})
+
     default:
       return state
   }
